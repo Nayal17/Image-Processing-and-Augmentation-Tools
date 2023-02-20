4f4a0258cb42dafc2900faa3c3d5790938197e77
Nayal17 <nayal17.916@gmail.com>
2023-02-20 18:31:08 +0530

    organized

--- convolve.py
+++ convolve.py
@@ -3,28 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm 
 
-def convolve(img, kernel, pad=0, stride=1):
-    channels = 1
-    try:
-        if img.shape[2]==3: # three channels
-            c1 = img[:,:,0]
-            c2 = img[:,:,1]
-            c3 = img[:,:,2]
-            channels=3
-    except:
-        pass
-
-    final_img = []
-
-    for c in range(channels):  
-        if channels>1:  
-            if c==0:
-                img = c1
-            if c==1:
-                img = c2
-            else:
-                img==c2
-            
+def convolve2d(img, kernel, pad=0, stride=1):
+        """takes image with only one channel"""
         img_height = img.shape[0]
         img_width = img.shape[1] 
         kernel_height = kernel.shape[0]
@@ -35,7 +15,7 @@ def convolve(img, kernel, pad=0, stride=1):
         out_img = np.zeros((out_img_height, out_img_width))
 
         if pad==0:
-            padded_img = pad
+            padded_img = img
         else:
             padded_img = np.zeros((img_height+pad*2, img_width+pad*2))
             padded_img[pad:pad+img_height, pad:pad+img_width] = img
@@ -52,11 +32,19 @@ def convolve(img, kernel, pad=0, stride=1):
                     if x%stride==0:
                         out_img[x,y] = (padded_img[x:x+kernel_height, y:y+kernel_width] * kernel).sum()
 
-        final_img.append(out_img)
+        return out_img
+
+def color_convolve(img, kernel, pad=0, stride=1):
+    """takes BGR image as input"""
+    b, g, r = cv2.split(img)
     
-    final_img = np.array(final_img).transpose(1,2,0)
-    
-    return final_img
+    b_conv = convolve2d(b, kernel) 
+    g_conv = convolve2d(g, kernel) 
+    r_conv = convolve2d(r, kernel) 
+
+    conv_bgr = cv2.merge([b_conv, g_conv, r_conv])
+
+    return conv_bgr
 
 
 if __name__=='__main__':
@@ -64,14 +52,16 @@ if __name__=='__main__':
     kernel = np.array([[0,-1,0],[-1,5,-1],[0,-1,0]])
 
     img = cv2.imread("/home/nayal/Documents/ImageProcessingTools/images/image.jpg")
-    rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    conv_img_g = convolve(gray, kernel, pad=1)
-    conv_img_rgb = convolve(rgb, kernel, pad=1)
 
+    gray_conv_img = convolve2d(gray, kernel, pad=1)
+    color_conv_img = color_convolve(img, kernel, pad=1)
+    
     plt.figure(figsize=(16,10))
-    plt.subplot(131);plt.imshow(rgb, cmap='gray') ; plt.title("RGB Image") 
-    plt.subplot(132);plt.imshow(conv_img_g, cmap="gray") ; plt.title("Gray Convolved Image") 
-    plt.subplot(133);plt.imshow(conv_img_rgb) ; plt.title("RGB Convolved Image") 
-
+    plt.subplot(131); plt.imshow(img); plt.title("BGR Image")
+    plt.subplot(132); plt.imshow(gray_conv_img, cmap="gray"); plt.title("Convolve2d Image")
+    plt.subplot(133); plt.imshow(color_conv_img); plt.title("Color Convolve Image")
     plt.show()
+
+
+    
